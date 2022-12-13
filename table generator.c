7e38fbd959638268e735f23de32d5315251549d0
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2022-12-13 22:19:44 +0100

    Update table generator.c

--- table generator.c
+++ table generator.c
@@ -13,17 +13,18 @@
     fprintf(fp,"\\begin{document}\n");
     for(int x = 0; x < 5; x++)
     {
+        fprintf(fp,"\\section{permutacja}\n");
         for(int z = 0; z < 5; z++)
         {
-            fprintf(fp,"\\begin{table}[]\n"); 
             fprintf(fp,"\\begin{tabular}{|c|c|}\n");
-            fprintf(fp,"\\hline\n");
+            fprintf(fp,"\\toprule\n");
             for(int y = 0; y < 5; y++)
             {
+                fprintf(fp,"\\midrule\n");
                 fprintf(fp," & (1,2,1,2,1) (1,2,1,2,1) (1,2,1,2,1) \\\\ \\hline\n");
             }
+            fprintf(fp,"\\bottomrule\n");
             fprintf(fp,"\\end{tabular}\n");
-            fprintf(fp,"\\end{table}\n");
         }
     }
     fprintf(fp,"\\end{document}\n");
